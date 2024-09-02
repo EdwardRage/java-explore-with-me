ea9eb632f4fba594117375a813c6a763eb4ced0f
Eduard <Obraen1399@yandex.ru>
2024-09-02 16:51:29 +0300

    попытка не пытка 3.2

--- stats-service/server/src/main/java/ru/practicum/ewm/StatRepository.java
+++ stats-service/server/src/main/java/ru/practicum/ewm/StatRepository.java
@@ -10,19 +10,19 @@ public interface StatRepository extends JpaRepository<StatModel, Long> {
 
     @Query("select new ru.practicum.ewm.StatResponseDto(eh.app, eh.uri, COUNT(eh.ip)) from StatModel as eh " +
             "where eh.uri in :uri and eh.timestamp between :start and :end " +
-            "group by eh.app " +
+            "group by eh.app, eh.uri " +
             "order by count(eh.ip) desc")
     List<StatResponseDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uri);
 
     @Query("select new ru.practicum.ewm.StatResponseDto(eh.app, eh.uri, COUNT(distinct eh.ip)) from StatModel as eh " +
             "where eh.uri in :uri and eh.timestamp between :start and :end " +
-            "group by eh.uri, eh.uri " +
+            "group by eh.app, eh.uri " +
             "order by count (distinct eh.ip) desc")
     List<StatResponseDto> getStatsWithUniqueIp(LocalDateTime start, LocalDateTime end, List<String> uri);
 
     @Query("select new ru.practicum.ewm.StatResponseDto(eh.app, eh.uri, COUNT(distinct eh.ip)) from StatModel as eh " +
             "where eh.timestamp between :start and :end " +
-            "group by eh.uri, eh.uri " +
+            "group by eh.app, eh.uri " +
             "order by count (distinct eh.ip) desc")
     List<StatResponseDto> getStatsWithoutUri(LocalDateTime start, LocalDateTime end);
 }
